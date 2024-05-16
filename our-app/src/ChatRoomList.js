828d758364b8388ea44d093034fdc268786cc16b
Khdscor <khds_cor@naver.com>
2024-05-16 23:03:23 +0900

    feat: React 채팅방 리스트 페이지 구현

---
+++ our-app/src/ChatRoomList.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const ChatRoomList = (props) => {
+
+    const [chatRoom, setChatRoom] = new useState([]);
+
+    const [inputValue, setInputValue] = useState('');
+   // 입력 필드에 변화가 있을 때마다 inputValue를 업데이트
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const fetchRooms = () => {
+        axios.get("http://localhost:8080/chatList" )
+            .then(response => {setChatRoom(response.data)});
+        };
+
+    const createRoom = () => {
+    if (inputValue) {
+        const body = {
+        title : inputValue
+        };
+        axios.post("http://localhost:8080/create", body)
+            .then(response => {
+                    if(response.status === 201){
+                        setInputValue('');
+                        setChatRoom((prev) => [...prev, response.data]);
+                    } else {
+                        alert("경고경고!");
+                    }
+                }
+            )
+        }
+    };
+
+
+    useEffect(() => {
+        fetchRooms();
+      }, []);
+    return (
+    <div>
+      <ul>
+        <div>
+          {/* 입력 필드 */}
+       <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      {/* 채팅방 추가 */}
+      <button onClick={createRoom}>입력</button>
+        </div>
+        {/* 채팅방 리스트 출력 */}
+        {chatRoom.map((item, index) => (
+          <div key={index} className="list-item">{item.title}</div>
+        ))}
+      </ul>
+    </div>
+    );
+};
+export default ChatRoomList;
